6085bc23bdae566f52d59023dc38b66b8f82c215
demirorens <demirorens@gmail.com>
2021-10-03 23:29:38 +0300

    yml dosyası

--- saNotesPostgres/src/main/java/com/sanotes/saNotesPostgres/Service/DAO/NoteBookRepository.java
+++ saNotesPostgres/src/main/java/com/sanotes/saNotesPostgres/Service/DAO/NoteBookRepository.java
@@ -1,7 +1,12 @@
 package com.sanotes.saNotesPostgres.Service.DAO;
 
 import com.sanotes.saNotesPostgres.Service.Model.NoteBookModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NoteBookRepository extends CrudRepository<NoteBookModel, Long> {
+    @Query("select n from NoteBookModel n where n.name = ?1")
+    List<NoteBookModel> findByName(String name);
 }
--- saNotesPostgres/src/main/java/com/sanotes/saNotesPostgres/Service/DAO/TagRepository.java
+++ saNotesPostgres/src/main/java/com/sanotes/saNotesPostgres/Service/DAO/TagRepository.java
@@ -1,7 +1,13 @@
 package com.sanotes.saNotesPostgres.Service.DAO;
 
 import com.sanotes.saNotesPostgres.Service.Model.TagModel;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TagRepository extends CrudRepository<TagModel, Long> {
+    @Query("select t from TagModel t where t.name like ?1")
+    List<TagModel> findByName(String name, Pageable pageable);
 }
